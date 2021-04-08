c16634d919309749c9a5761d256efe56796a3657
Hayden Carpenter <93haydcarp@csdecatur.net>
2021-04-08 15:25:15 +0000

    Test'

--- convert.py
+++ convert.py
@@ -2,9 +2,6 @@ import imgkit
 
 
 
-path_wkthmltoimage = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage.exe'
-config = imgkit.config(wkhtmltoimage=path_wkthmltoimage)
-
-imgkit.from_url('https://codedecatur.org', 'out.jpg', config=config)
+imgkit.from_url('https://codedecatur.org', 'out.jpg')
 #imgkit.from_file('test.html', 'out.jpg')
 #imgkit.from_string('Hello!', 'out.jpg')
