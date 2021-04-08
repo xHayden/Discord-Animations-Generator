ed6bc9da73e23a2af5c78d8968ac65ad4fdf7be8
xHayden <93haydcarp@csdecatur.net>
2021-04-08 12:30:59 -0400

    testing

--- convert.py
+++ convert.py
@@ -7,7 +7,7 @@ def create_snapshots():
             'javascript-delay': str((i * 10) + 1000),
             'allow': "."
         }
-        imgkit.from_file('roulette.html', 'out{0}.jpg'.format(i), options=options)
+        imgkit.from_file(r'/home/Discord-Animations-Generator/roulette.html', 'out{0}.jpg'.format(i), options=options)
 create_snapshots()
 #imgkit.from_url('https://codedecatur.org', 'out.jpg')
 
