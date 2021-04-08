a612077e91ff64272c0eee0daaae72bb6041ac3c
Hayden Carpenter <93haydcarp@csdecatur.net>
2021-04-08 16:55:44 +0000

    yes

--- convert.py
+++ convert.py
@@ -5,9 +5,14 @@ def create_snapshots():
         options = {
             'format': 'png',
             'javascript-delay': str((i * 10) + 1000),
-            'allow': "."
+            'allow': ".",
+	    'debug-javascript': None,
+	    'cookie': [
+		('target', '22'),
+	    ],
+	    'enable-local-file-access': None
         }
-        imgkit.from_file(r'/home/Discord-Animations-Generator/roulette.html', 'out{0}.jpg'.format(i), options=options)
+        imgkit.from_file('roulette.html', 'out{0}.png'.format(i), options=options)
 create_snapshots()
 #imgkit.from_url('https://codedecatur.org', 'out.jpg')
 
