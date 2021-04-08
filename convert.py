6419b7b84c159db72c06d94f2d48d05caf783daf
Hayden Carpenter <93haydcarp@csdecatur.net>
2021-04-08 16:02:13 +0000

    images

--- convert.py
+++ convert.py
@@ -1,10 +1,10 @@
 import imgkit
 
 def create_snapshots():
-    for i in range(100):
+    for i in range(1):
         options = {
             'format': 'png',
-            'javascript-delay': str(i * 10),
+            'javascript-delay': str((i * 10) + 1000),
             'allow': "."
         }
         imgkit.from_file('roulette.html', 'out{0}.jpg'.format(i), options=options)
