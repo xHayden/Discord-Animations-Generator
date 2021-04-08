daa518792d99fafde9c8a1a3223e58f0d4ffa79a
xHayden <93haydcarp@csdecatur.net>
2021-04-08 11:47:03 -0400

    testing

--- convert.py
+++ convert.py
@@ -1,7 +1,13 @@
 import imgkit
 
+def create_snapshots():
+    for i in range(100):
+        options = {
+            'format': 'png',
+            'javascript-delay': i * 10,
+        }
+        imgkit.from_file('roulette.html', 'out{0}.jpg'.format(i), options=options)
+create_snapshots()
+#imgkit.from_url('https://codedecatur.org', 'out.jpg')
 
-
-imgkit.from_url('https://codedecatur.org', 'out.jpg')
-#imgkit.from_file('test.html', 'out.jpg')
 #imgkit.from_string('Hello!', 'out.jpg')
